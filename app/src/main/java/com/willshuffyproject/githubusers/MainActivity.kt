7a2daa35ac93f5d33dace184b612dd37e85a5bc8
willshuffy <saputrawilly26@gmail.com>
2020-10-15 18:11:06 +0700

    ListView Transition

--- app/src/main/java/com/willshuffyproject/githubusers/MainActivity.kt
+++ app/src/main/java/com/willshuffyproject/githubusers/MainActivity.kt
@@ -2,11 +2,24 @@ package com.willshuffyproject.githubusers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //load animation
+
+        val apps_logo = AnimationUtils.loadAnimation(this, R.anim.alpha_with_scale)
+        val text_logo = AnimationUtils.loadAnimation(this, R.anim.alpha_with_scale)
+
+
+        //run animation
+
+        iv_logo.startAnimation(apps_logo)
+        tv_logo.startAnimation(text_logo)
     }
 }
